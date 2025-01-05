aad5ca6a1ae9a959985c4fb12ec789a3a60bac23
Viktor Lofgren <vlofgren@marginalia.nu>
2025-01-05 16:52:35 +0100

    Fix generation

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        nicotine: '#f8f8ee',
+        margeblue: '#3e5f6f',
+        liteblue: '#0066cc',
+      }
+    },
+    screens: {
+      'xs': '440px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+  },
+  plugins: [],
+}
+
